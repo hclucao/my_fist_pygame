e0e4900e207e9c571dfb99555b19c065c14e23ee
hclucao <hclucao@outlook.com>
2021-06-22 17:27:26 -0300

    agora com os objetos do mundo no word.py

--- player.py
+++ player.py
@@ -7,16 +7,8 @@ dm = dirmestre.Diretorio()
 
 sprite_sheet = pygame.image.load(dm.perso()) #loading the spritesheet of the player
 
-word = pygame.Rect(200, 200, 1000,1000)
-tile = pygame.Rect(280, 1150, 200, 30)
-
-#test of draw tiles by loop for
-blocks = [pygame.Rect(100,100, 50,50), pygame.Rect(160,100, 50,50),
-        pygame.Rect(220, 100, 50, 50)
-        ]
-
 class Player(pygame.sprite.Sprite):
-    def __init__(self,):
+    def __init__(self):
         """todo que envolve o personagem
         """
         self.left = False
@@ -44,47 +36,38 @@ class Player(pygame.sprite.Sprite):
 
         #movimentacao
         player_movement = [0,0]
-        word_movement = [0,0]
+        self.word_movement = [0,0]
         if self.right and self.left == True:
             self.walkingr(0)
         else:
             if self.right == True:
                 self.walkingr(32)
                 if self.pos_x > 400:
-                    word_movement[0] -= 5
+                    self.word_movement[0] -= 5
                 else:
-                    player_movement[0] += 3; word_movement[0] -= 5
+                    player_movement[0] += 3; self.word_movement[0] -= 5
             else: self.walkingr(0)
             ###self.left moviment
             if self.left == True:
                 self.walkingr(64)
                 if self.pos_x < 200:
-                    word_movement[0] += 5
+                    self.word_movement[0] += 5
                 else:
-                    player_movement[0] -= 3; word_movement[0] += 5
+                    player_movement[0] -= 3; self.word_movement[0] += 5
             ###self.up moviment
         if self.up == True:
             if self.pos_y > 100:
                 player_movement[1] -= 10
             else:
-                word_movement[1] += 10
+                self.word_movement[1] += 10
                 
         #gravidade  
-        if self.rect.colliderect(tile):
-            pass
+        if self.pos_y < 410:
+            self.pos_y += 5
         else:
-            if self.pos_y < 410:
-                self.pos_y += 5
-            else:
-                word_movement[1] -= 5
+            self.word_movement[1] -= 5
         
         self.pos_x += player_movement[0]; self.pos_y += player_movement[1]
-        word.x += word_movement[0]; word.y += word_movement[1]
-        tile.x += word_movement[0]; tile.y += word_movement[1]
-
-        for block in blocks:
-            block.copy()
-            block.x += word_movement[0]; block.y += word_movement[1]
 
     def walkingr(self, index):
         pygame.sprite.Sprite.__init__(self)
